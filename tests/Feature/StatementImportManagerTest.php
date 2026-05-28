05db42f474192ae4e36e5eae613787d1e5d04d55
yoni-haber <yhaber@regatta.com>
2026-05-28 11:40:43 +0100

    use qualifying imports

--- tests/Feature/StatementImportManagerTest.php
+++ tests/Feature/StatementImportManagerTest.php
@@ -17,8 +17,11 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Storage;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 use Livewire\Livewire;
+use Livewire\WithFileUploads;
 use ReflectionAttribute;
 use ReflectionClass;
 use Tests\TestCase;
@@ -622,11 +625,11 @@ final class StatementImportManagerTest extends TestCase
         $titleAttribute = null;
 
         foreach ($attributes as $attribute) {
-            if ($attribute->getName() === \Livewire\Attributes\Layout::class) {
+            if ($attribute->getName() === Layout::class) {
                 $layoutAttribute = $attribute;
             }
 
-            if ($attribute->getName() === \Livewire\Attributes\Title::class) {
+            if ($attribute->getName() === Title::class) {
                 $titleAttribute = $attribute;
             }
         }
@@ -641,7 +644,7 @@ final class StatementImportManagerTest extends TestCase
     {
         $statementImportManager = new StatementImportManager();
 
-        $this->assertContains(\Livewire\WithFileUploads::class, class_uses($statementImportManager));
+        $this->assertContains(WithFileUploads::class, class_uses($statementImportManager));
     }
 
     public function test_rules_method_validates_with_authenticated_user(): void
