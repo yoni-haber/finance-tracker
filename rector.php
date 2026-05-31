2e7d0c95e7ce586ce801386e5357833fa3d0f818
yoni-haber <jonathanhaber.jh@gmail.com>
2026-05-31 14:57:49 +0100

    php 8.5

--- rector.php
+++ rector.php
@@ -3,11 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
 use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
+    ->withPhpVersion(PhpVersion::PHP_85)
     ->withPaths([
-        __DIR__.'/app',
+//        __DIR__.'/app',
         __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
